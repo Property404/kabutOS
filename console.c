9ef98ae253be3cc798908d88019c6bf1934c6917
Dawn Spoerr <dawnspoerr@tranetechnologies.com>
2023-11-12 08:36:40 -0600

    added parse array function to count the num of args entered by a user.

--- console.c
+++ console.c
@@ -4,6 +4,7 @@
 #include "functions.h"
 #include "string.h"
 #include "stdio.h"
+#include <stdbool.h>
 
 #define INDEX_SIZE 256
 
@@ -32,24 +33,44 @@ void resetArray() {
 	index = 0;
 }
 
+// TODO, use array of arguments and pass that
+// aaray of char ptrs
+
 void parseArray() {
-    print("Parsing Array...\r\n");
+    // return on empty enter press
+    if (input_array[0] == '\0') {
+        return;
+    }
+    
+    // TODO: fix this issue so I can set to 0
+    int numArgs = -1;
     char* str;
-    int numArgs = 0;
-    char* koala = input_array;
+
+    // had really weird issues referencing
+    // array directly, so cast to ptr here
+    char* arrayptr = input_array;
+
+    // using a dowhile as str would start null otherwise
     do {
-	    // grab the first command
-        printArray();
-        print ("Debug 2\r\n");
-        str = strsep(&koala, " ");
+        str = strsep(&arrayptr, " ");
         numArgs++;
-        print("DEBUG!\r\n");
     }
     while(str != NULL);
 
+    // DEBUG
+    // TODO, we should pass this out
     print("Num of arguments: ");
     printf("%x", numArgs);
-    print("\r\n");
+
+    // the value of input_array we're left with
+    // points to just the first word
+    // the spaces have been replaced with nulls
+    // so we don't need to copy the command
+    // instead we can just investigate the
+    // value of that word
+    // so we can add a command here to 
+    // parseCommand, which will use the 
+    // function lookup table
 }
 
 void storeArray(unsigned char c) {
@@ -57,14 +78,22 @@ void storeArray(unsigned char c) {
 
         // check for ENTER key
         if (current_char == '\r') {
+            // TODO: because we parse the array
+            // before we print it, we end up 
+            // printing just the first word
+            // which is unintuitive
+            // but i'm leaving this as is while we
+            // get console.c function call working :)
             parseArray();
             resetArray();
+            return;
         }
 
         // max size of the array.
         if (index >= INDEX_SIZE) {
-                print("Array maxed out. Oops!\r\n");
-		resetArray();
+            print("Array maxed out. Oops!\r\n");
+		    resetArray();
+            return;
         }
 
         input_array[index] = current_char;
