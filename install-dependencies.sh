d3547460ccd0e90d938c8387f7f9f0f34004036d
Property404 <git@dagans.dev>
2023-11-13 16:46:10 -0600

    Improve deps install script

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,9 +1,51 @@
 #!/usr/bin/env bash
-sudo apt-get install \
-    vim tmux git \
-    gcc-riscv64-unknown-elf \
-    binutils-riscv64-unknown-elf \
-    qemu-system-riscv64 \
-    gdb-multiarch
-rustup target add riscv64imac-unknown-none-elf
+# Install dependencies needed for KabutOS development
 
+main() {
+    local -r USAGE="Usage: $(basename "${0}")"
+    local -r HELP="Install KabutOS dependencies
+
+$USAGE
+
+Help:
+    --ci        Install for CI instead of developer"
+
+    local ci
+    while true; do
+        case "$1" in
+            --ci) ci=1; shift ;;
+            -h | --help ) echo "$HELP"; return 0 ;;
+            -- ) shift; break ;;
+            -* ) echo -e "Unrecognized option: $1\n$USAGE" >&2; return 1 ;;
+            * ) break ;;
+        esac
+    done
+
+    if ! command -v apt > /dev/null; then
+        echo "This command expects Debian or Ubuntu" >&2
+        return 1
+    fi
+
+    local packages=" gcc-riscv64-unknown-elf "
+
+    # CI doesn't need quite as much
+    if [[ -z "${ci}" ]]; then
+        packages+=" vim tmux git "
+        packages+=" binutils-riscv64-unknown-elf "
+        packages+=" qemu-system-riscv64 "
+        packages+=" gdb-multiarch "
+    fi
+
+    # Install RustUp if needed
+    if ! command -v rustup > /dev/null; then
+        curl --proto '=https' --tlsv1.3 -sSf https://sh.rustup.rs | sh
+    fi
+
+    # Install Rust Targets
+    rustup target add riscv64imac-unknown-none-elf
+
+    # shellcheck disable=SC2086
+    sudo apt install ${packages}
+}
+
+main "${@}"
