2ce1f425d5b908fd1b4fb91e5cf86aff4d505349
Property404 <git@dagans.dev>
2023-11-13 16:46:12 -0600

    remove useless function

--- krabby/src/lib.rs
+++ krabby/src/lib.rs
@@ -3,11 +3,6 @@ use core::panic::PanicInfo;
 pub mod c_functions;
 pub mod drivers;
 
-#[no_mangle]
-pub fn snorkel() -> i32 {
-    4
-}
-
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
