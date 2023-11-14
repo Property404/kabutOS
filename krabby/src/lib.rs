2ef0ad461c5c23debe275be3086d601a39781191
Property404 <git@dagans.dev>
2023-11-13 19:39:30 -0600

    Add better panic handler

--- krabby/src/lib.rs
+++ krabby/src/lib.rs
@@ -6,7 +6,11 @@ pub mod c_functions;
 pub mod drivers;
 pub mod serial;
 
+use core::fmt::Write;
+use serial::Serial;
+
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    let _ = writeln!(Serial::new(), "\x1b[31mKERNEL PANIC!\x1b[0m {}", info);
     loop {}
 }
