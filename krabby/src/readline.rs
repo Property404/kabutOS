7464cd3594d579952bf1d7477828a1296e8204bc
Property404 <git@dagans.dev>
2023-11-16 18:08:12 -0600

    Start rewriting rust readline

--- krabby/src/readline.rs
+++ krabby/src/readline.rs
@@ -1,25 +1,35 @@
 //! GNU Readline-like functionality
 use crate::errors::KernelResult;
 use crate::serial::Serial;
-use core::fmt::Write;
+use core::{fmt::Write, str};
 
 /// Read line of user input
 pub fn get_line(buffer: &mut [u8]) -> KernelResult<&str> {
     let mut serial = Serial::new();
+
+    // This points to the current *byte* in the array, which may or may be different from the
+    // current *character* since *characters* are variable length
     let mut byte_ptr = 0;
 
     loop {
-        let c = serial.next_char()?;
-        if c == '\r' {
-            let _ = writeln!(serial);
-            return Ok(core::str::from_utf8(buffer).unwrap());
-        }
-        if byte_ptr + c.len_utf8() < buffer.len() && !c.is_ascii_control() {
-            if !c.is_ascii_control() {
-                let _ = write!(serial, "{c}");
-                c.encode_utf8(&mut buffer[byte_ptr..]);
+        match serial.next_char()? {
+            // <Enter> is pressed. Print a new line and return
+            '\r' => {
+                writeln!(serial)?;
+                return Ok(core::str::from_utf8(buffer)?);
             }
-            byte_ptr += c.len_utf8();
+
+            // Ignore all other control characters
+            c if c.is_ascii_control() => {}
+
+            // Character is entered - Echo and place on buffer
+            c => {
+                if byte_ptr + c.len_utf8() < buffer.len() {
+                    write!(serial, "{c}")?;
+                    c.encode_utf8(&mut buffer[byte_ptr..]);
+                    byte_ptr += c.len_utf8();
+                }
+            }
         }
     }
 }
