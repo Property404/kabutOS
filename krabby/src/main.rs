2bca1e4cce50be97fe198c801058d57e50f10ba9
Property404 <git@dagans.dev>
2023-11-18 16:12:45 -0600

    kmain: get hard id and device tree pointer from bootloader

--- krabby/src/main.rs
+++ krabby/src/main.rs
@@ -7,13 +7,14 @@ use krabby::{
 };
 
 #[no_mangle]
-unsafe fn kmain() {
+unsafe fn kmain(_hart_id: usize, fdt_ptr: *const u8) {
     // Initialize drivers
     let uart_driver = Ns16550Driver::new(0x10000000 as *mut u8);
     unsafe { DRIVERS.uart = Some(uart_driver) };
 
     let mut serial = Serial::new();
     writeln!(serial, "Welcome to KabutOS!!!").unwrap();
+    writeln!(serial, "Device tree is @ {fdt_ptr:p}").unwrap();
 
     loop {
         unsafe {
