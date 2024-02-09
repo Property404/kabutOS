3c86399e5a57efcff84547a3b968dd17c3ef1cf7
Property404 <git@dagans.dev>
2024-02-08 19:00:22 -0600

    Add rundbg script

---
+++ rundbg.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Run QEMU with the intent to debug
+
+# Build Krabby
+CARGO_OUTPUT=target/riscv64gc-unknown-none-elf/debug/krabby
+cargo build
+
+# Make a bin file because if we use the elf file QEMU will want to load it at
+# the intended virtual address because it's stupid or something
+riscv64-unknown-elf-objcopy -O binary ${CARGO_OUTPUT}{,.bin}
+
+qemu-system-riscv64 -serial mon:stdio -nographic -machine virt -bios none -kernel \
+    "${CARGO_OUTPUT}.bin" -S -s
