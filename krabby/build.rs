e877aac664d183f0cf0cf877b0a956b8cb161e58
Property404 <git@dagans.dev>
2024-02-12 19:23:49 -0600

    Rebuild if linker script changes

--- krabby/build.rs
+++ krabby/build.rs
@@ -1,6 +1,8 @@
 // This is a build script - it runs before any Rust code is compiled
 fn main() {
+    const LINKER_SCRIPT: &str = "./linker.ld";
     // Set linker script
     println!("cargo:rustc-link-arg=-T");
-    println!("cargo:rustc-link-arg=./linker.ld");
+    println!("cargo:rustc-link-arg={LINKER_SCRIPT}");
+    println!("cargo:rerun-if-changed={LINKER_SCRIPT}");
 }
