7c1f751005fb4a6efa989008afa9584173d9d14b
Property404 <git@dagans.dev>
2024-03-14 17:47:46 -0500

    Rename function

--- krabby/src/test.rs
+++ krabby/src/test.rs
@@ -15,10 +15,10 @@ pub fn test_and_exit() {
 fn test_inner() -> KernelResult<()> {
     crate::util::test();
 
-    quit()
+    quit_qemu()
 }
 
-fn quit() -> KernelResult<()> {
+fn quit_qemu() -> KernelResult<()> {
     let addr = mmu::map_device(TEST_ADDRESS, 0x1000)?;
     let handle = qemu_exit::RISCV64::new(addr as u64);
     handle.exit_success();
