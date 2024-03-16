57188cfb83a215048367dfcf27697cfcb8b4a225
Property404 <git@dagans.dev>
2024-03-15 19:58:06 -0500

    delint shell scripts

--- run.sh
+++ run.sh
@@ -9,5 +9,5 @@ cargo build
 # Make a bin file because if we use the elf file QEMU will want to load it at
 # the intended virtual address because it's stupid or something
 riscv64-unknown-elf-objcopy -O binary ${CARGO_OUTPUT}{,.bin}
-qemu-system-riscv64 -serial mon:stdio -nographic -machine virt -bios none ${@}  -kernel \
+qemu-system-riscv64 -serial mon:stdio -nographic -machine virt -bios none "${@}" -kernel \
     "${CARGO_OUTPUT}.bin"
